8a2902b0ca05286ab97825ca3692b18a1d14c53a
BrunoSousa <bmmvs_@msn.com>
2022-12-09 13:43:49 +0000

    streamlit labels9

--- app/app_how_green.py
+++ app/app_how_green.py
@@ -192,8 +192,8 @@ with st.form(key='params_for_api'):
         plt.title("Hidroelectric Production")
 
         if begin_date >= 1675:
-            plt.plot(df_final['pred'][begin_date-100:begin_date],label='REAL');
-            plt.plot(df_final['energie_ma'][begin_date-1:begin_date+48],ls='--',label='PREDICTION');
+            plt.plot(df_final['pred'][begin_date-100:begin_date],ls='--',label='Prediction');
+            plt.plot(df_final['energie_ma'][begin_date-1:begin_date+48],label='Real');
             plt.legend(loc='upper left', fontsize=8);
         else:
             plt.plot(df_final['pred'][begin_date-30:begin_date+48],label='PREDICTION');
